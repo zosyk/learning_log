e5528023323f5e4c2cc4ab1e9735a3ac9e764d88
zosyk <alekcandrzosyk@gmail.com>
2018-04-23 08:59:05 +0300

    added first template (home page index.html)

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """ The home page for learning log"""
+    return render(request, 'learning_logs/index.html')
