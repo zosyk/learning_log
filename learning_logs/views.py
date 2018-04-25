441e703df634d427245f290eac3cae1ba326688f
zosyk <alekcandrzosyk@gmail.com>
2018-04-25 06:50:52 +0300

    added topics page

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,9 +2,19 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Topic
+
 
 # Create your views here.
 
 def index(request):
     """ The home page for learning log"""
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """ Show the all topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+
+    return render(request, 'learning_logs/topics.html', context)
