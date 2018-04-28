1b5d7635324388c555824cde7f89ddaa4fd03312
zosyk <alekcandrzosyk@gmail.com>
2018-04-28 07:21:04 +0300

    added login/logout functionality

--- users/views.py
+++ users/views.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import logout
 
 # Create your views here.
+def logout_view(request):
+    """ Log the user out. """
+    logout(request)
+
+    return HttpResponseRedirect(reverse('learning_logs:index'))
